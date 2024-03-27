6d119e0e06ec08fec372d4db424a93a28a5aadb5
Iordan Bogdanov <iordanbogdanov88@gmail.com>
2024-03-27 23:27:10 +0200

    added styles to core module components

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords-validator';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidator(['com', 'bg'])]],
+    tel: [''],
+    passGroup: this.fb.group(
+      {
+        password: ['', [Validators.required]],
+        rePassword: ['', [Validators.required]],
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
+  });
 
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const {
+      username,
+      email,
+      tel,
+      passGroup: { password, rePassword } = {},
+    } = this.form.value;
+  }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,17 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { UserRoutingModule } from './user-routing.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+  imports: [CommonModule, UserRoutingModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
-export class UserModule { }
+export class UserModule {}
